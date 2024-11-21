71a52b692858035e7d832a829d062978f7f5bb25
Gerson_CursoDWeb <chancasevangelistagerson@gmail.com>
2024-11-21 18:56:40 -0500

    pregunta 1

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,23 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/p2",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      //{ path: "movies", component: () => import("pages/PageMovie.vue") },
+    ],
+  },
+  {
+    path: "/",
+    component: () => import("src/components/Login/LoginForm.vue"),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
